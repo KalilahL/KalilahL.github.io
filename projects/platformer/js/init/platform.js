92d7c2108c6d2d6823975bafd638b0c07778f00b
KalilahL <cadetklewis@nomma.net>
2022-10-24 18:21:29 +0000

    first day

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -34,12 +34,14 @@
      * asset, giving it a scaleX and scaleY of 2 will stretch it across the
      * bottom of the game.
      */
-    createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
+    createPlatform(0, game.world.height - 40, 12, 12); // DO NOT DELETE
 
     // example:
     createPlatform(400, 250);
-    createPlatform(170, 460, 0.5);
-
+    createPlatform(450, 560, 0.5);
+    createPlatform(400,400);
+    createPlatform(150,480,0.5)
+    createPlatform(85,350,.5)
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
